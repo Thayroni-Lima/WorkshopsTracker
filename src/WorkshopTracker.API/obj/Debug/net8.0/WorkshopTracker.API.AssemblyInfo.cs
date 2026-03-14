fe5a6d7b9d1ec20fb63382629522eb21898314bc
Thayroni Lima <130916851+Thayroni-Lima@users.noreply.github.com>
2026-03-14 09:12:02 -0300

    [BACK] remover serviços e endpoints não utilizados da API (weatherforecast)

--- src/WorkshopTracker.API/obj/Debug/net8.0/WorkshopTracker.API.AssemblyInfo.cs
+++ src/WorkshopTracker.API/obj/Debug/net8.0/WorkshopTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkshopTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd573dbe15e42cf8bd8c9f4b20492771a03ac9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkshopTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkshopTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
