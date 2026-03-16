f4d82b43722448dc41377c7df679e40e0ef22096
Thayroni Lima <130916851+Thayroni-Lima@users.noreply.github.com>
2026-03-16 16:01:50 -0300

    [FRONT][SERVICES] Criação dos Services de Colaborador e Workshop | Importação do provedor de HttpClient para fazer requisições HTTP

---
+++ front/workshop-tracker/src/app/services/colaborador.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Colaborador } from '../models/colaborador.model';
+import { Workshop } from '../models/workshop.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ColaboradorService {
+  private apiUrl = 'http://localhost:5064/api/colaboradores';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Colaborador[]> {
+    return this.http.get<Colaborador[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Colaborador> {
+    return this.http.get<Colaborador>(`${this.apiUrl}/${id}`);
+  }
+
+  create(colaborador: Colaborador): Observable<Colaborador> {
+    return this.http.post<Colaborador>(this.apiUrl, colaborador);
+  }
+
+  update(id: number, colaborador: Colaborador): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, colaborador);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  getWorkshopsByColaboradorId(colaboradorId: number): Observable<Workshop[]> {
+    return this.http.get<Workshop[]>(`${this.apiUrl}/${colaboradorId}/workshops`);
+  }
+}
---
+++ front/workshop-tracker/src/app/services/workshop.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Colaborador } from '../models/colaborador.model';
+import { Workshop } from '../models/workshop.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WorkshopService {
+  private apiUrl = 'http://localhost:5064/api/workshops';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Workshop[]> {
+    return this.http.get<Workshop[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Workshop> {
+    return this.http.get<Workshop>(`${this.apiUrl}/${id}`);
+  }
+
+  create(workshop: Workshop): Observable<Workshop> {
+    return this.http.post<Workshop>(this.apiUrl, workshop);
+  }
+
+  update(id: number, workshop: Workshop): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, workshop);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  getColaboradoresByWorkshopId(workshopId: number): Observable<Colaborador[]> {
+    return this.http.get<Colaborador[]>(`${this.apiUrl}/${workshopId}/colaboradores`);
+  }
+
+  postColaboradorToWorkshop(workshopId: number, colaboradorId: number): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/${workshopId}/colaboradores/${colaboradorId}`, {});
+  }
+
+  deleteColaboradorFromWorkshop(workshopId: number, colaboradorId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${workshopId}/colaboradores/${colaboradorId}`);
+  }
+}
