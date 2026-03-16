dcacb7636cb7106ce2265dac5583f7fcacb46290
Thayroni Lima <130916851+Thayroni-Lima@users.noreply.github.com>
2026-03-16 17:09:13 -0300

    [FRONT][PAGES][COLABORADORES] GetAllColaboradores funcional

--- front/workshop-tracker/src/app/pages/colaboradores/colaboradores.ts
+++ front/workshop-tracker/src/app/pages/colaboradores/colaboradores.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ColaboradorService } from '../../core/services/colaborador.service';
+import { Colaborador } from '../../models/colaborador.model';
 
 @Component({
   selector: 'app-colaboradores',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './colaboradores.html',
   styleUrl: './colaboradores.css',
 })
-export class Colaboradores {}
+
+export class Colaboradores implements OnInit {
+  colaboradores = signal<Colaborador[]>([]);
+  loading = signal(true);
+  error = signal('');
+
+  constructor(private colaboradorService: ColaboradorService) {}
+
+  ngOnInit(): void {
+    this.colaboradorService.getAll().subscribe({
+      next: (data) => {
+        this.colaboradores.set(data);
+        this.loading.set(false);
+      },
+      error: (err) => {
+        this.error.set('Erro ao carregar colaboradores.');
+        this.loading.set(false);
+        console.error(err);
+      },
+    });
+  }
+}
