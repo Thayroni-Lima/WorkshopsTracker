230d9dd5e078b48bc05ce7e69f89ce368d45eb21
Thayroni Lima <130916851+Thayroni-Lima@users.noreply.github.com>
2026-03-17 16:32:26 -0300

    [PAGES][PAGES][COLABORADORES] Implementa associação e desassociação de Workshops a um Colaborador | ajusta acessos

--- front/workshop-tracker/src/app/pages/colaboradores/colaborador-workshops/colaborador-workshops.ts
+++ front/workshop-tracker/src/app/pages/colaboradores/colaborador-workshops/colaborador-workshops.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ColaboradorService } from '../../../core/services/colaborador.service';
+import { WorkshopService } from '../../../core/services/workshop.service';
 import { Workshop } from '../../../models/workshop.model';
 
 @Component({
@@ -11,28 +12,55 @@ import { Workshop } from '../../../models/workshop.model';
   styleUrl: './colaborador-workshops.css',
 })
 export class ColaboradorWorkshops implements OnInit {
+  colaboradorId = signal<number>(0);
   workshops = signal<Workshop[]>([]);
+  todosWorkshops = signal<Workshop[]>([]);
   loading = signal(true);
   error = signal('');
 
+  disponiveis = computed(() =>
+    this.todosWorkshops().filter((w) => !this.workshops().some((a) => a.id === w.id)),
+  );
+
   constructor(
     private colaboradorService: ColaboradorService,
+    private workshopService: WorkshopService,
     private route: ActivatedRoute,
   ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.colaboradorId.set(id);
+    this.carregarDados();
+  }
 
-    this.colaboradorService.getWorkshopsByColaboradorId(id).subscribe({
+  carregarDados(): void {
+    this.loading.set(true);
+
+    this.colaboradorService.getWorkshopsByColaboradorId(this.colaboradorId()).subscribe({
       next: (data) => {
         this.workshops.set(data);
         this.loading.set(false);
       },
-      error: (err) => {
-        this.error.set('Erro ao carregar workshops do colaborador.');
-        this.loading.set(false);
-        console.error(err);
-      },
+      error: () => this.error.set('Erro ao carregar workshops do colaborador.'),
+    });
+
+    this.workshopService.getAll().subscribe({
+      next: (data) => this.todosWorkshops.set(data),
+    });
+  }
+
+  adicionar(workshopId: number): void {
+    this.workshopService.postColaboradorToWorkshop(workshopId, this.colaboradorId()).subscribe({
+      next: () => this.carregarDados(),
+      error: () => this.error.set('Erro ao adicionar workshop.'),
+    });
+  }
+
+  remover(workshopId: number): void {
+    this.workshopService.deleteColaboradorFromWorkshop(workshopId, this.colaboradorId()).subscribe({
+      next: () => this.carregarDados(),
+      error: () => this.error.set('Erro ao remover workshop.'),
     });
   }
 }
